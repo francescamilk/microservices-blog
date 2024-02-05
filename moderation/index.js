09ae804bee9680f39755f09556c0a678447d0c41
Francesca Santoriello <f.santoriello@hotmail.com>
2024-02-05 13:12:33 +0100

    moderation process CommentCreated, send CommentModerated

--- moderation/index.js
+++ moderation/index.js
@@ -5,8 +5,25 @@ import axios from 'axios';
 const app = express();
 app.use(bodyParser.json())
 
-app.post('/events', (req, res) => {
-    console.log('Event Received: ', req.body.type);
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('forbidden') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                content: data.content,
+                postId: data.postId,
+                status
+            }
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    }
+
     res.send({});
 });
 
